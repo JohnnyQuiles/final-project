a88c1a4f94cb7bdab4c4429f9e2672b874c4953e
Johnny Quiles <79813390+JohnnyQuiles@users.noreply.github.com>
2021-12-10 11:39:06 -0500

    Adding more animals/ style features

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-
+//FLIP CARDS TO SHOW ANIMAL
 const cardBack = document.querySelector('.cards-back');
 
 cardBack.addEventListener('click', function() {
-    function flipCard () {
-        card.classList.toggle("flipCard"); 
-    }
-    flipCard();
+
 }) 
